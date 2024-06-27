65dfe3149fb6db76084d7ccb280e42088c12c08b
Mobin Azadani <azad.1214mobin@gmail.com>
2024-06-27 22:50:39 +0330

    nfa done not tested

--- phase0/FA_class.py
+++ phase0/FA_class.py
@@ -1,6 +1,4 @@
 import json
-
-
 class State:
     __counter = 0
 
@@ -22,13 +20,13 @@ class State:
 
 
 class DFA:
-    state_ids: dict[int] = {}
 
     def __init__(self) -> None:
+        self.state_ids: dict[int, 'State'] = {}
         self.init_state = None
         self.states: list['State'] = []
-        self.alphabet: list['str'] = []
-        # self.final_states: list['State'] = []
+        self.alphabet: set['str'] = []
+        # self.initial_states: list['State'] = []
         self.final_states: set['State'] = []
 
     @staticmethod
@@ -36,7 +34,7 @@ class DFA:
         fa = DFA()
         json_fa = json.loads(json_str)
 
-        fa.alphabet = json_fa["alphabet"]
+        fa.alphabet = set(json_fa["alphabet"])
 
         for state_str in json_fa["states"]:
             fa.add_state(int(state_str[2:]))
@@ -71,20 +69,21 @@ class DFA:
     def add_state(self, id: int | None = None) -> State:
         new_state = State(id | None)
         self.states.append(new_state)
-        DFA.state_ids[new_state.id] = new_state
+        self.state_ids[new_state.id] = new_state
         return new_state
 
     def add_transition(self, from_state: State, to_state: State, input_symbol: str) -> None:
         from_state.add_transition(input_symbol, to_state)
 
-    def assign_initial_state(self, state: State) -> None:
-        ...
+    # def assign_initial_state(self, state: State) -> None:
+    #     self.initial_states.add(state)
+        
 
     def add_final_state(self, state: State) -> None:
         self.final_states.add(state)
 
     def get_state_by_id(self, id) -> State | None:
-        return DFA.state_ids[id]
+        return self.state_ids[id]
 
     def is_final(self, state: State) -> bool:
         return state in self.final_states
@@ -113,27 +112,78 @@ class NFAState:
 
 class NFA:
     def __init__(self) -> None:
-        self.init_state = None
-        self.states: list['State'] = []
-        self.alphabet: list['str'] = []
+        self.init_state: 'NFAState' = None
+        self.states: list['NFAState'] = []
+        self.alphabet: set['str'] = []
         # self.final_states: list['State'] = []
-        self.final_states: set['State'] = []
+        self.final_states: set['NFAState'] = []
+        self.state_ids: dict[int, 'NFAState'] = {}
 
     @staticmethod
     def convert_DFA_instance_to_NFA_instance(dfa_machine: 'DFA') -> 'NFA':
-        ...
+        nfa = NFA()
+        nfa.alphabet = dfa_machine.alphabet + ['λ']
+        
+        for id in dfa_machine.state_ids.keys():
+            nfa.add_state(id)
+
+        nfa.init_state = nfa.state_ids[dfa_machine.init_state.id]
+        nfa.final_states = [nfa.state_ids[state.id] for state in dfa_machine.final_states]
+        for state in dfa_machine.states:
+            for key in state.transitions.keys():
+                to_state = state.transitions[key]
+                nfa.state_ids[state.id].add_transition(key, nfa.state_ids[to_state.id])
+        return nfa
     
     @staticmethod
     def union(machine1: 'NFA', machine2: 'NFA') -> 'NFA':
-        ...
-    
+        machine1.alphabet = machine1.alphabet.union(machine2.alphabet)
+        offset = len(machine1.state_ids)
+        for id in machine2.state_ids.keys():
+            machine1.state_ids[id + offset] = machine2.state_ids[id]
+            machine2.state_ids[id].id += offset
+        machine1.init_state.add_transition('λ', machine2.init_state)
+        return machine1
+
     @staticmethod
     def concat(machine1: 'NFA', machine2: 'NFA') -> 'NFA':
-        ...
+
+        machine1.alphabet = machine1.alphabet.union(machine2.alphabet)
+        offset = len(machine1.state_ids)
+
+        for id in machine2.state_ids.keys():
+            machine1.state_ids[id + offset] = machine2.state_ids[id]
+            machine2.state_ids[id].id += offset
+
+        for state in machine1.final_states:
+            state.add_transition('λ', machine2.init_state)
+
+        machine1.final_states = machine2.final_states
+        return machine1
     
     @staticmethod
     def star(machine: 'NFA') -> 'NFA':
-        ...
+        machine.final_states.add(machine.init_state)
+        for state in machine.final_states:
+            state.add_transition('λ',machine.init_state)
 
+    def add_state(self, id: int | None = None) -> NFAState:
+        new_state = NFAState(id | None)
+        self.states.append(new_state)
+        self.state_ids[new_state.id] = new_state
+        return new_state
+    
     def serialize_to_json(self) -> str:
-        ...
+        fa = {
+            "states": list(map(lambda s: f"q_{s.id}", self.states)),
+            "initial_state": f"q_{self.init_state.id}",
+            "final_states": list(map(lambda s: f"q_{s.id}", self.final_states)),
+            "alphabet": self.alphabet
+        }
+
+        for state in self.states:
+            fa[f"q_{state.id}"] = {}
+            for symbol in self.alphabet:
+                fa[f"q_{state.id}"][symbol] = f"q_{state.transitions[symbol].id}"
+
+        return json.dumps(fa)
---
+++ utils/test.py
@@ -0,0 +1,5 @@
+import utils
+import pathlib
+
+r = utils.convert_pictures_to_gray_scale_and_binary_array(r'C:\Users\ASUS\Desktop\code\TLA\project-template\data\module1Test\1.png')
+print((r[1]))
